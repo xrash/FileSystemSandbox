1073cd748f51cd5075270e49ee5dd182d1a49a24
Felipe Gonçalves <felipe@xra.sh>
2021-10-14 18:19:50 -0300

    Add multipart upload

--- App.tsx
+++ App.tsx
@@ -15,7 +15,7 @@ import {
   FileSystemUploadType,
 } from 'expo-file-system'
 
-const SERVER_URL = 'https://2cb9-2804-431-c7c1-e74f-fa28-19ff-febe-5f69.ngrok.io/upload'
+const SERVER_BASE_URL = 'http://7167-2804-431-c7c1-e74f-fa28-19ff-febe-5f69.ngrok.io'
 
 const pickFile = async () => {
   console.log('pickFile begin')
@@ -40,43 +40,76 @@ const pickFile = async () => {
   return imageResult
 }
 
-const uploadFile = async (uri: string) => {
-  console.log('uploadFile begin')
-  const resp = await uploadAsync(SERVER_URL, uri, {
+const uploadFileBinary = async (uri: string) => {
+  console.log('uploadFileBinary begin')
+  console.log('uploadFileBinary uri', uri)
+  const resp = await uploadAsync(`${SERVER_BASE_URL}/upload-binary`, uri, {
     httpMethod: 'PUT',
     uploadType: FileSystemUploadType.BINARY_CONTENT,
   })
-  console.log('resp', resp)
-  console.log('uploadFile end')
+  console.log('uploadFileBinary resp', resp)
+  console.log('uploadFileBinary end')
+}
+
+const uploadFileMultipart = async (uri: string) => {
+  console.log('uploadFileMultipart begin')
+  console.log('uploadFileMultipart uri', uri)
+  const resp = await uploadAsync(`${SERVER_BASE_URL}/upload-multipart`, uri, {
+    httpMethod: 'PUT',
+    uploadType: FileSystemUploadType.MULTIPART,
+    fieldName: 'file',
+  })
+  console.log('uploadFileMultipart resp', resp)
+  console.log('uploadFileMultipart end')
 }
 
 export default function App() {
 
-  const handlePressUploadFile = async () => {
-    console.log('handlePressUploadFile begin')
+  const handlePressUploadBinary = async () => {
+    console.log('handlePressUploadBinary begin')
 
     try {
       const file = await pickFile()
       const { uri } = file
-      console.log('handlePressUploadFile uri', uri)
-      await uploadFile(uri)
+      await uploadFileBinary(uri)
     } catch (e) {
-      console.log('caught exception', e)
+      console.log('exception', e)
     }
 
-    console.log('handlePressUploadFile end')
+    console.log('handlePressUploadBinary end')
+  }
+
+  const handlePressUploadMultipart = async () => {
+    console.log('handlePressUploadMultipart begin')
+
+    try {
+      const file = await pickFile()
+      const { uri } = file
+      await uploadFileMultipart(uri)
+    } catch (e) {
+      console.log('exception', e)
+    }
+
+    console.log('handlePressUploadMultipart end')
   }
 
   return (
     <View style={styles.container}>
       <View style={{ height: 256 }}></View>
-      <Text>Upload a file.</Text>
-      <Button
-        title="upload file"
-        onPress={handlePressUploadFile}
-      />
+      <View style={{ margin: 32 }}>
+        <Button
+          title="upload file binary"
+          onPress={handlePressUploadBinary}
+        />
+      </View>
+      <View style={{ margin: 32 }}>
+        <Button
+          title="upload file multipart"
+          onPress={handlePressUploadMultipart}
+        />
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -84,4 +117,4 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'green',
   },
-});
+})
--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"io"
 )
 
 var tempdir = os.TempDir()
@@ -14,8 +15,10 @@ func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "hello\n")
 }
 
-func upload(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("Got /upload")
+func uploadBinary(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Got /upload-binary")
+
+	fmt.Println("Method", req.Method)
 
 	for name, headers := range req.Header {
 		for _, h := range headers {
@@ -44,8 +47,47 @@ func upload(w http.ResponseWriter, req *http.Request) {
 	fmt.Printf("Written %d bytes to file %s\n", written, tempfile.Name())
 }
 
+func uploadMultipart(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Got /upload-multipart")
+
+	fmt.Println("Method", req.Method)
+
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Printf("%v: %v\n", name, h)
+		}
+	}
+
+    err := req.ParseMultipartForm(10 * 1024 * 1024)
+	if err != nil {
+		fmt.Println("Error parsing multipart request", err)
+		return
+	}
+
+    file, _, err := req.FormFile("file")
+    if err != nil {
+		fmt.Println("Error getting data from multipart field", err)
+		return
+    }
+
+	tempfile, err := ioutil.TempFile(tempdir, "__file_uploaded")
+	if err != nil {
+		fmt.Println("Error creating temp file", err)
+		return
+	}
+
+	written, err := io.Copy(tempfile, file)
+	if err != nil {
+		fmt.Println("Error writing file", err)
+		return
+	}
+
+	fmt.Printf("Written %d bytes to file %s\n", written, tempfile.Name())
+}
+
 func main() {
 	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/upload", upload)
+	http.HandleFunc("/upload-binary", uploadBinary)
+	http.HandleFunc("/upload-multipart", uploadMultipart)
 	http.ListenAndServe(":7778", nil)
 }
