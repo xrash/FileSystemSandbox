1e4920430c20cdd9d0e2bbf7cf56b1a66ae61cf7
Felipe Gonçalves <felipe@xra.sh>
2021-10-14 17:03:54 -0300

    1st commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+} from 'react-native'
+import {
+  MediaTypeOptions,
+  launchImageLibraryAsync,
+  requestMediaLibraryPermissionsAsync,
+} from 'expo-image-picker'
+import {
+  uploadAsync,
+  FileSystemUploadType,
+} from 'expo-file-system'
+
+const SERVER_URL = 'https://2cb9-2804-431-c7c1-e74f-fa28-19ff-febe-5f69.ngrok.io/upload'
+
+const pickFile = async () => {
+  console.log('pickFile begin')
+
+  const permissionResult = await requestMediaLibraryPermissionsAsync()
+
+  if (!permissionResult.granted) {
+    throw new Error('permission denied')
+  }
+
+  const imageResult = await launchImageLibraryAsync({
+    mediaTypes: MediaTypeOptions.All,
+    base64: true,
+  })
+
+  if (imageResult.cancelled) {
+    throw new Error('picker cancelled')
+  }
+
+  console.log('pickFile end')
+
+  return imageResult
+}
+
+const uploadFile = async (uri: string) => {
+  console.log('uploadFile begin')
+  const resp = await uploadAsync(SERVER_URL, uri, {
+    httpMethod: 'PUT',
+    uploadType: FileSystemUploadType.BINARY_CONTENT,
+  })
+  console.log('resp', resp)
+  console.log('uploadFile end')
+}
 
 export default function App() {
+
+  const handlePressUploadFile = async () => {
+    console.log('handlePressUploadFile begin')
+
+    try {
+      const file = await pickFile()
+      const { uri } = file
+      console.log('handlePressUploadFile uri', uri)
+      await uploadFile(uri)
+    } catch (e) {
+      console.log('caught exception', e)
+    }
+
+    console.log('handlePressUploadFile end')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ height: 256 }}></View>
+      <Text>Upload a file.</Text>
+      <Button
+        title="upload file"
+        onPress={handlePressUploadFile}
+      />
     </View>
   );
 }
@@ -14,8 +82,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'green',
   },
 });
---
+++ server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+	"io/ioutil"
+	"os"
+)
+
+var tempdir = os.TempDir()
+
+func hello(w http.ResponseWriter, req *http.Request) {
+    fmt.Println("Got /hello")
+    fmt.Fprintf(w, "hello\n")
+}
+
+func upload(w http.ResponseWriter, req *http.Request) {
+    fmt.Println("Got /upload")
+
+    for name, headers := range req.Header {
+        for _, h := range headers {
+            fmt.Printf("%v: %v\n", name, h)
+        }
+    }
+
+	b, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		fmt.Println("Error reading body", err)
+		return
+	}
+
+	tempfile, err := ioutil.TempFile(tempdir, "__file_uploaded")
+	if err != nil {
+		fmt.Println("Error creating temp file", err)
+		return
+	}
+
+	written, err := tempfile.Write(b)
+	if err != nil {
+		fmt.Println("Error writing file", err)
+		return
+	}
+
+	fmt.Printf("Written %d bytes to file %s\n", written, tempfile.Name())
+}
+
+func main() {
+    http.HandleFunc("/hello", hello)
+    http.HandleFunc("/upload", upload)
+    http.ListenAndServe(":7778", nil)
+}
